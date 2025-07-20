9e2008be45e1c02d0af6f3d3553dda29cd9a3c7b
Justin-Qian <yujia@skiplegal.com>
2025-07-20 00:58:01 -0400

    add black for python format

--- backend/src/backend/routers/users.py
+++ backend/src/backend/routers/users.py
@@ -6,6 +6,7 @@ from backend.routers.auth import get_current_user
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+
 @router.get("/me")
 async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
     return current_user
