2c0712e63944a3115753d42e41eb056b41b33ef2
Justin-Qian <yujia@skiplegal.com>
2025-07-21 00:59:02 -0400

    update session

--- backend/src/backend/models.py
+++ backend/src/backend/models.py
@@ -1,26 +1,72 @@
-from sqlalchemy import String, ForeignKey, Integer, Boolean
+from sqlalchemy import JSON, DateTime, String, ForeignKey, Integer, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from backend.database import Base
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
+import datetime
+
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4
+    )
     name: Mapped[str] = mapped_column(String, nullable=False, index=True)
     email: Mapped[str] = mapped_column(String, index=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String,nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
     files: Mapped[list["File"]] = relationship(back_populates="user")
+    chat_sessions: Mapped[list["ChatSession"]] = relationship(back_populates="user")
+
 
 class File(Base):
     __tablename__ = "files"
 
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
     name: Mapped[str] = mapped_column(String, nullable=False)
-    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    name: Mapped[str] = mapped_column(String,nullable=False)
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("users.id"), nullable=False
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False)
     content_type: Mapped[str] = mapped_column(String, nullable=False)
     size: Mapped[int] = mapped_column(Integer, nullable=False)
     is_indexed: Mapped[bool] = mapped_column(Boolean, nullable=True, default=False)
     user: Mapped["User"] = relationship(back_populates="files")
+
+
+class ChatSession(Base):
+    __tablename__ = "chat_sessions"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("users.id")
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, default=datetime.datetime.now
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, default=datetime.datetime.now
+    )
+    user: Mapped["User"] = relationship(back_populates="chat_sessions")
+    messages: Mapped[list["ChatMessage"]] = relationship(back_populates="session")
+
+
+class ChatMessage(Base):
+    __tablename__ = "chat_messages"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    session_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("chat_sessions.id")
+    )
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, default=datetime.datetime.now
+    )
+    content: Mapped[dict] = mapped_column(JSON, nullable=False)
+    session: Mapped["ChatSession"] = relationship(back_populates="messages")
--- backend/src/backend/routers/users.py
+++ backend/src/backend/routers/users.py
@@ -2,11 +2,13 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from backend.models import User
-from backend.routers.auth import get_current_user
+from backend.routers.auth import get_current_user_from_cookie
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.get("/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_user_from_cookie)],
+):
     return current_user
